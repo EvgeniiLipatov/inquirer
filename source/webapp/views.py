3fed564b36605c250a3de801bb4adedce83bab1b
Evgenii <lipatowevgeniy@gmail.com>
2019-11-16 14:52:01 +0600

    The second stage. Added CRUD for Products

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,3 +1,42 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse, reverse_lazy
+from django.views.generic import DetailView, ListView, CreateView, UpdateView, DeleteView
 
-# Create your views here.
+from webapp.models import Product
+
+
+class IndexView(ListView):
+    model = Product
+    template_name = 'index.html'
+
+
+class ProductView(DetailView):
+    model = Product
+    template_name = 'product/detail.html'
+
+
+class ProductCreateView(CreateView):
+    model = Product
+    template_name = 'product/create.html'
+    fields = ('name', 'category', 'photo', 'description')
+
+    def get_success_url(self):
+        return reverse('webapp:product_detail', kwargs={'pk': self.object.pk})
+
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    template_name = 'product/update.html'
+    fields = ('name', 'category', 'photo', 'description')
+    context_object_name = 'product'
+
+    def get_success_url(self):
+        return reverse('webapp:product_detail', kwargs={'pk': self.object.pk})
+
+
+class ProductDeleteView(DeleteView):
+    model = Product
+    template_name = 'product/delete.html'
+    success_url = reverse_lazy('webapp:index')
+    context_object_name = 'product'
